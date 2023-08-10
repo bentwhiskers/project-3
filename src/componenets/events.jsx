b7473158f9cc35f8af5e4c42907c9fcb1f2f4690
carolinebea <coraline.bea@gmail.com>
2023-08-10 19:15:54 -0400

    added components for header and navbar

---
+++ src/componenets/events.jsx
@@ -0,0 +1,12 @@
+const Events = () => {
+  return (
+   <div>
+    <h2>Events Page</h2>
+    <div className = "Events">
+    
+    </div>
+   </div>
+  );
+};
+
+export default Events;
---
+++ src/componenets/home.jsx
@@ -0,0 +1,15 @@
+import Spotlight from "./spotlight";
+
+const Home = () => {
+  return (
+   <div>
+    <h2>Home Page</h2>
+    <div className = "Home">
+    <p>Stardew community board vibes</p>
+    <Spotlight></Spotlight>
+   </div>
+   </div>
+  );
+};
+
+export default Home;
