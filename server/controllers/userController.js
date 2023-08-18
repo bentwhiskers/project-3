bb493a097e5a7c5737f981be7c07c7abfb959a25
jake <mobleyj2@gmail.com>
2023-08-17 21:18:06 -0400

    add the models and controllers files

---
+++ server/controllers/userController.js
@@ -0,0 +1,29 @@
+const { User, Application } = require('../models');
+
+module.exports = {
+
+  // create a new user
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Delete a user and associated apps
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      await Application.deleteMany({ _id: { $in: user.applications } });
+      res.json({ message: 'User and associated apps deleted!' })
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
--- server/models/Events.js
+++ server/models/Events.js
@@ -1,21 +1,31 @@
-const mongoose = ('mongoose');
+import mongoose from 'mongoose';
 
-const eventSchema = new mongoose.Schema({
+const { Schema } = mongoose;
+
+const eventSchema = new Schema({
     title: {
-
+        type: String,
+        minLength: 3,
+        maxLength: 160,
     },
     description: {
-
+        type: String,
+        minLength: 5,
+        maxLength: 100,
     },
     date: {
-
+        type: Date,
+        default: Date.now,
     },
     time: {
-
+        type: String,
+        minLength: 3,
+        maxLength: 160,
     },
-    tags: {
-
-    }
+    tags: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Tag', //  match the name of your tag schema
+    }],
 });
 
 const Event = mongoose.model('Event', eventSchema);
