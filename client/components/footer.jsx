01e1cfba1db9cf5eda16bdc679b94a189a2d0ca8
bentwhiskers <abbott1414@gmail.com>
2023-08-20 19:41:24 -0400

    added styling to footer.jsx and added fontawesome to package.json

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -2,10 +2,13 @@
 import { jsx } from '@emotion/react';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const ListStyle = styled.li`
   color: white;
   font-weight: bold;
+  padding: .5em;
   
 `;
 
@@ -23,10 +26,10 @@ export default function Footer() {
 
       }}>Contact the Creators:</h4> 
       <ul>
-        <ListStyle>Caroline B: <LinkStyle>github.com/carolinebea</LinkStyle></ListStyle>
-        <ListStyle>Erica A: <LinkStyle>github.com/bentwhiskers</LinkStyle></ListStyle>
-        <ListStyle>Jake M: <LinkStyle>github.com/mobleyj2</LinkStyle></ListStyle>
-        <ListStyle>David G: <LinkStyle>github.com/Gcod0</LinkStyle></ListStyle>
+        <ListStyle>Caroline B: <LinkStyle>github.com/carolinebea <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></LinkStyle></ListStyle>
+        <ListStyle>Erica A: <LinkStyle>github.com/bentwhiskers <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></LinkStyle></ListStyle>
+        <ListStyle>Jake M: <LinkStyle>github.com/mobleyj2 <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></LinkStyle></ListStyle>
+        <ListStyle>David G: <LinkStyle>github.com/Gcod0 <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></LinkStyle></ListStyle>
       </ul>
      </div>
     );
